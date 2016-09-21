b02693af4fd3cf0272933b47a2a6fc3c63736c6c
Hao <CJHao@corp.tcc.inet>
2016-09-21 09:41:52 +0800

    add eshell startup

--- funcs.el
+++ funcs.el
@@ -7,3 +7,26 @@ Version 2015-06-12"
     (switch-to-buffer ξbuf)
     (funcall (and initial-major-mode))
     (setq buffer-offer-save t)))
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
--- keybindings.el
+++ keybindings.el
@@ -1,1 +1,2 @@
 (global-set-key (kbd "<f7>") 'xah-new-empty-buffer)
+(global-set-key (kbd "C-!") 'eshell-here)
